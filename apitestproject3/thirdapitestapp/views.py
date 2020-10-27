ad0f87b7928728ab701bce6be9d922e9e25abff3
prashant the psk <73543132+prashantthepsk007@users.noreply.github.com>
2020-10-28 00:23:51 +0530

    Update views.py

--- apitestproject3/thirdapitestapp/views.py
+++ apitestproject3/thirdapitestapp/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 1.this the view of the app of project 1 st push and commit and first changes in views
+
+2. this is the second commit from github to pycharm ide 
