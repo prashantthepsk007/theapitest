23410ae789f6fbc8cc636f5a184e715e5adc5a61
khapekar.prashant007@gmail.com <khapekar.prashant007@gmail.com>
2020-10-28 00:25:13 +0530

    second from pycharm views

--- apitestproject3/thirdapitestapp/views.py
+++ apitestproject3/thirdapitestapp/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 1.this the view of the app of project 1 st push and commit and first changes in views
 
 2. this is the second commit from github to pycharm ide 
+
+3. this is the third commit of project from pycharm to git hub
