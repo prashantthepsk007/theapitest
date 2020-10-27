8e241ec51fe8e1ba13ba32402c701f22376a052b
khapekar.prashant007@gmail.com <khapekar.prashant007@gmail.com>
2020-10-28 00:28:49 +0530

    third  from pycharm views to github

--- apitestproject3/thirdapitestapp/views.py
+++ apitestproject3/thirdapitestapp/views.py
@@ -8,3 +8,6 @@ from django.shortcuts import render
 3. this is the third commit of project from pycharm to git hub
 
 4. this is the fourth commit from the githun to pycharm how the things going on guys 
+
+
+5. thasts all around us has been suffered from chinese virus and thing has been sluken but now its all good telll us abou t yourself buddy
